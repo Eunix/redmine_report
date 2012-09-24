8b8f85e79261f7cca5a4b961203d9b3d09087419
Evgeny Pavlov <evgeny@pavlov.name>
2012-09-24 17:26:24 +0400

    Table with sum of issues and spent time

--- app/controllers/time_reports_controller.rb
+++ app/controllers/time_reports_controller.rb
@@ -1,10 +1,13 @@
 class TimeReportsController < ApplicationController
   unloadable
 
+
   def index
   end
 
   def create
     @reports = Report.get(params[:group], params[:week])
+
+    render :layout => 'report'
   end
 end

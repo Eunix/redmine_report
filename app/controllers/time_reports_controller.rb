0d36103bfb62ab8ce99be6fb1f9787a34d4f92e8
Evgeny Pavlov <evgeny@pavlov.name>
2012-09-24 16:34:51 +0400

    Collector of spent time data. View with HTML table.

--- app/controllers/time_reports_controller.rb
+++ app/controllers/time_reports_controller.rb
@@ -1,11 +1,10 @@
 class TimeReportsController < ApplicationController
   unloadable
 
-
   def index
   end
 
   def create
-
+    @reports = Report.get(params[:group], params[:week])
   end
 end
--- lib/report.rb
+++ lib/report.rb
@@ -1,41 +1,55 @@
 # encoding: utf-8
 class Report
-  def self.get(group, week)
+  def self.get(group_id, week)
+    group = Group.find(group_id)
     our_field = IssueCustomField.where(name: "Тип задачи").first
-    issues = Hash.new
+    report = Array.new
+    week_begin = Date.commercial(2012, week.to_i, 1)
+    week_end = Date.commercial(2012, week.to_i, 5)
 
     # People
     group.users.each do |user|
-      # Name
-      issues[user.name] = {}
-
       # Spent time
-      time_entries = TimeEntry.where(user_id: user.id, tweek: week)
+      days = {}
 
-      time_entries.each do |time_entry|
-        issue = time_entry.issue
+      (week_begin..week_end).each do |day|
+        time_entries = TimeEntry.where(user_id: user.id, spent_on: day)
 
-        # Type of issue (from our custom field)
-        issue_type = CustomValue.where(customized_type: "Issue", customized_id: issue.id, custom_field_id: our_field.id).first
-        if issue_type
-          issue_type = issue_type.value
-        else
-          issue_type = "Неизвестно"
+        # Issues
+        issues = {}
+
+        time_entries.each do |time_entry|
+          issue = time_entry.issue
+
+          # Type of issue (from our custom field)
+          issue_type = CustomValue.where(customized_type: "Issue", customized_id: issue.id, custom_field_id: our_field.id).first
+          if issue_type
+            issue_type = issue_type.value
+          else
+            issue_type = "Неизвестно"
+          end
+
+          unless issues[issue_type]
+            issues[issue_type] = {}
+          end
+
+          # Issue info
+          if issues[issue_type][issue.id]
+            issues[issue_type][issue.id] = issues[issue_type][issue.id] + time_entry.hours
+          else
+            issues[issue_type][issue.id] = time_entry.hours
+          end
         end
 
-        unless issues[user.name][issue_type]
-          issues[user.name][issue_type] = {}
-        end
-
-        # Issue info
-        if issues[user.name][issue_type][issue.id]
-          issues[user.name][issue_type][issue.id] = issues[user.name][issue_type][issue.id] + time_entry.hours
-        else
-          issues[user.name][issue_type][issue.id] = time_entry.hours
-        end
+        days[day] = issues
       end
+
+      report << {
+          :user_name => user.name,
+          :days => days
+      }
     end
 
-    issues
+    report
   end
 end

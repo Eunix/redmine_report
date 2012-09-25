5bc9f8ba4833fe4a55ccf465381a80bff8b44b0c
Evgeny Pavlov <evgeny@pavlov.name>
2012-09-25 11:28:11 +0400

    Show tr of issue

--- app/helpers/time_reports_helper.rb
+++ app/helpers/time_reports_helper.rb
@@ -1,9 +1,21 @@
 module TimeReportsHelper
   # Special helper to color an issue dependeing of its status
-  def show_issue_tr(issue_array)
+  def show_issue_tr(issue_array, day)
+    #date = Date.parse(day)
+    issue = Issue.find(issue_array[0])
+    if issue.closed_date
+      if issue.closed_date.to_date > day
+        issue_color = "#ffffdd" # yellow (in_process)
+      else
+        issue_color = "#dfffdf" # green (closed)
+      end
+    else
+      issue_color = "#ffffdd" # yellow (in_process)
+    end
+
     html  = <<HTML
     <tr>
-        <td class="issue_name">#{Issue.find(issue_array[0]).subject}</td>
+        <td class="issue_name" style="background-color:#{issue_color}">#{issue.subject}</td>
         <td class="spent_time">#{issue_array[1]}</td>
     </tr>
 HTML

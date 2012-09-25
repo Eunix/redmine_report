e5cdb593bd0f94c97343af4dc265a9ffac5145a6
Evgeny Pavlov <evgeny@pavlov.name>
2012-09-25 10:59:49 +0400

    View helper to show issue
    Helper helps to show different type of issues (closed, in process)

--- app/helpers/time_reports_helper.rb
+++ app/helpers/time_reports_helper.rb
@@ -1,2 +1,12 @@
 module TimeReportsHelper
+  # Special helper to color an issue dependeing of its status
+  def show_issue_tr(issue_array)
+    html  = <<HTML
+    <tr>
+        <td class="issue_name">#{Issue.find(issue_array[0]).subject}</td>
+        <td class="spent_time">#{issue_array[1]}</td>
+    </tr>
+HTML
+    html.html_safe
+  end
 end
